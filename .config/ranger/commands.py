199bba3ff7b461794aecb0d2ca9cabc473b6716d
Peter Nguyen <peter@mictis.com>
2022-02-02 15:02:21 +0100

    feat(ranger): add command pdfunite to merge pdf files

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -62,6 +62,36 @@ from ranger.core.loader import CommandLoader
 #         return self._tab_directory_content()
 #
 
+class pdfunite(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        original_path = cwd.path
+        parts = self.line.split()
+        target_filename = parts[1:]
+
+        descr = "merge pdf files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['pdfunite'] + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files] + target_filename, descr=descr, read=True)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+    def tab(self, tabnum):
+        """ Complete with current folder name """
+
+        extension = ['.pdf']
+        return ['pdfunite ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
+
 class compress(Command):
     def execute(self):
         """ Compress marked files to current directory """
