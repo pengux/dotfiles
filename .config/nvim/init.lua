b8e39b35765c9029df11e8c728e2749bc0b2be22
Peter Nguyen <peter@mictis.com>
2022-03-18 14:26:26 +0100

    feat(nvim): use float windows for running external processes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -728,6 +728,7 @@ require("dap-go").setup()
 --go.nvim
 require("go").setup({
 	dap_debug_keymap = true,
+	run_in_floaterm = true,
 })
 -- Import on save
 vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
