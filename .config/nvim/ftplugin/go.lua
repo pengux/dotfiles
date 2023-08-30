cdd4858c5c10b541264e300c52bbb7c41ee9b76e
Peter Nguyen <peter@mictis.com>
2023-08-30 08:25:18 +0200

    nvim: move file type specific config to files in ftplugin/

---
+++ .config/nvim/ftplugin/go.lua
@@ -0,0 +1,15 @@
+local keymap_opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<leader>a", ":GoAlt<CR>", keymap_opts)
+
+function Goimports()
+  local original_cursor = vim.fn.winsaveview()
+  vim.cmd([[%!goimports]])
+  vim.fn.winrestview(original_cursor)
+end
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = Goimports,
+  group = format_sync_grp,
+})
+
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,8 +29,6 @@ require("packer").startup(function()
   use("mattn/emmet-vim")
   -- UI to select things (files, grep results, open buffers...)
   use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } })
-  -- Add indentation guides even on blank lines
-  use("lukas-reineke/indent-blankline.nvim")
   use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
   -- use {
   --   'TimUntersberger/neogit',
@@ -338,12 +336,6 @@ vim.g.indent_blankline_char = "┊"
 vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-
--- Format JSON using jq. Use command 'gq%'
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "json",
-  command = "set formatprg=jq",
-})
 
 -- Mini
 require('mini.ai').setup()
@@ -815,19 +807,6 @@ require("go").setup({
   dap_debug_keymap = true,
   run_in_floaterm = true,
   -- tag_transform = true,
-})
-
-function Goimports()
-  local original_cursor = vim.fn.winsaveview()
-  vim.cmd([[%!goimports]])
-  vim.fn.winrestview(original_cursor)
-end
-
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = Goimports,
-  group = format_sync_grp,
 })
 
 --Spectre
