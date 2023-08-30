3dbbebca8e9f9eddbc98d599efe421ba356d83e4
Peter Nguyen <peter@mictis.com>
2023-08-30 15:41:34 +0200

    nvim: remove unused file and fix autocmd for GoImport

--- .config/nvim/ftplugin/go.lua
+++ .config/nvim/ftplugin/go.lua
@@ -9,6 +9,7 @@ end
 
 local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
   callback = Goimports,
   group = format_sync_grp,
 })
