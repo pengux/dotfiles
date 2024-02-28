8417c235ebcdb5c9f6405e6a382b23972b027225
Peter Nguyen <peter@mictis.com>
2024-02-28 10:25:00 +0100

    nvim: add vale and gen.nvim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -167,6 +167,12 @@ require("packer").startup(function()
       'nvim-lua/plenary.nvim'
     }
   }
+  use {
+    'marcelofern/vale.nvim'
+  }
+  use {
+    'David-Kunz/gen.nvim'
+  }
 end)
 
 --Set highlight on search
@@ -894,3 +900,33 @@ require('vgit').setup({
     }
   }
 })
+
+-- vale
+require("vale").setup({
+  -- path to the vale binary.
+  bin="/usr/bin/vale",
+  -- path to your vale-specific configuration.
+  vale_config_path="$HOME/.config/vale/vale.ini",
+})
+
+require('gen').setup({
+  model = "zephyr", -- The default model to use.
+  host = "localhost", -- The host running the Ollama service.
+  port = "11434", -- The port on which the Ollama service is listening.
+  display_mode = "float", -- The display mode. Can be "float" or "split".
+  show_prompt = false, -- Shows the Prompt submitted to Ollama.
+  show_model = false, -- Displays which model you are using at the beginning of your chat session.
+  no_auto_close = false, -- Never closes the window automatically.
+  init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+  -- Function to initialize Ollama
+  command = function(options)
+    return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/generate -d $body"
+  end,
+  -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+  -- This can also be a command string.
+  -- The executed command must return a JSON object with { response, context }
+  -- (context property is optional).
+  -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+  debug = false -- Prints errors and the command which is run.
+})
+vim.keymap.set({ 'n', 'v' }, '<leader>ai', ':Gen<CR>')
