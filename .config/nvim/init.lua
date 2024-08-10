c952ea0e65603f218e1a0eacc20ab3c21a7c539d
Peter Nguyen <peter@mictis.com>
2024-08-10 11:12:41 +0200

    nvim: enable marksman for markdown lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -633,7 +633,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 --Enable the following language servers
-local servers = { "terraformls", "graphql", "golangci_lint_ls", "eslint", "tsserver", "jsonnet_ls" }
+local servers = { "terraformls", "graphql", "golangci_lint_ls", "eslint", "tsserver", "jsonnet_ls", "marksman" }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup({
     on_attach = on_attach,
